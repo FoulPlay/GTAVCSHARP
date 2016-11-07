37b4752b26af1e3094f4f33406239a2e51fb62ae
Nathan Binks <FoulPlay@users.noreply.github.com>
2016-11-07 13:06:30 +0000

    Fixed some format issues

--- AI-Scripts/Cop_Spawner.cs
+++ AI-Scripts/Cop_Spawner.cs
@@ -12,8 +12,8 @@ using GTA.Native;
 
 namespace CopSpawner
 {
-    public class Main : Script
-    {
+	public class Main : Script
+	{
 		public Main()
 		{
 			this.Tick += onTick;
@@ -33,18 +33,18 @@ namespace CopSpawner
 		{
 			if (e.KeyCode == Keys.D0)
 			{
-                //CreateCop(); //Just for testing, it creates a cop
-                CreateCopsandVehicle(); //Creates a cop car and cops in it
+				//CreateCop(); //Just for testing, it creates a cop
+				CreateCopsandVehicle(); //Creates a cop car and cops in it
 			}
-            if (e.KeyCode == Keys.D9)
-            {
-                ChangeCOPRL(); //Change the relation groups between the gangs and cops
-            }
-            if (e.KeyCode == Keys.D8)
-            {
-                CreateSwatandVehicle();
-            }
-        }
+			if (e.KeyCode == Keys.D9)
+			{
+				ChangeCOPRL(); //Change the relation groups between the gangs and cops
+			}
+			if (e.KeyCode == Keys.D8)
+			{
+				CreateSwatandVehicle();
+			}
+		}
 
 		void ChangeCOPRL()
 		{
@@ -53,40 +53,40 @@ namespace CopSpawner
 			World.SetRelationshipBetweenGroups(Relationship.Dislike, Function.Call<int>(Hash.GET_HASH_KEY, "COP"), Function.Call<int>(Hash.GET_HASH_KEY, "AMBIENT_GANG_BALLAS"));
 		}
 
-        void CreateSwatandVehicle()
-        {
-            //Creates the swat armored truck (The model (HASH), where it spawns in the world (VECTOR 3))
-            Vehicle swatVeh = World.CreateVehicle(VehicleHash.Dilettante, Game.Player.Character.GetOffsetInWorldCoords(new GTA.Math.Vector3(0, 10, 0)));
-            swatVeh.MarkAsNoLongerNeeded();
-            swatVeh.PlaceOnGround();
-            //Creats the driver and passengers (seat, model (HASH))
-            Ped swatDriver = swatVeh.CreatePedOnSeat(VehicleSeat.Driver, PedHash.Cop01SFY);
-            Ped swatPassengerRightFront = swatVeh.CreatePedOnSeat(VehicleSeat.RightFront, PedHash.Cop01SMY);
-            Ped swatPassengerLeftRear = swatVeh.CreatePedOnSeat(VehicleSeat.LeftRear, PedHash.Cop01SMY);
-            Ped swatPassengerRightRear = swatVeh.CreatePedOnSeat(VehicleSeat.RightRear, PedHash.Cop01SFY);
-            //Give the driver and passengers weapons (Weapon (HASH), ammo, equp the weapon, is ammo loaded in clip)
-            swatDriver.Weapons.Give(WeaponHash.CombatPDW, 500, false, false);
-            swatDriver.Weapons.Give(WeaponHash.CombatPistol, 500, false, false);
-            swatPassengerRightFront.Weapons.Give(WeaponHash.CombatPDW, 500, false, false);
-            swatPassengerRightFront.Weapons.Give(WeaponHash.CombatPistol, 500, false, false);
-            swatPassengerLeftRear.Weapons.Give(WeaponHash.CombatPDW, 500, false, false);
-            swatPassengerLeftRear.Weapons.Give(WeaponHash.CombatPistol, 500, false, false);
-            swatPassengerRightRear.Weapons.Give(WeaponHash.PumpShotgun, 500, false, false);
-            swatPassengerRightRear.Weapons.Give(WeaponHash.CombatPistol, 500, false, false);
-            //Marks them "no longer needed" them so GTA V AI system takes over them
-            swatDriver.MarkAsNoLongerNeeded();
-            swatPassengerRightFront.MarkAsNoLongerNeeded();
-            swatPassengerLeftRear.MarkAsNoLongerNeeded();
-            swatPassengerRightRear.MarkAsNoLongerNeeded();
-        }
+		void CreateSwatandVehicle()
+		{
+			//Creates the swat armored truck (The model (HASH), where it spawns in the world (VECTOR 3))
+			Vehicle swatVeh = World.CreateVehicle(VehicleHash.Dilettante, Game.Player.Character.GetOffsetInWorldCoords(new GTA.Math.Vector3(0, 10, 0)));
+			swatVeh.MarkAsNoLongerNeeded();
+			swatVeh.PlaceOnGround();
+			//Creats the driver and passengers (seat, model (HASH))
+			Ped swatDriver = swatVeh.CreatePedOnSeat(VehicleSeat.Driver, PedHash.Cop01SFY);
+			Ped swatPassengerRightFront = swatVeh.CreatePedOnSeat(VehicleSeat.RightFront, PedHash.Cop01SMY);
+			Ped swatPassengerLeftRear = swatVeh.CreatePedOnSeat(VehicleSeat.LeftRear, PedHash.Cop01SMY);
+			Ped swatPassengerRightRear = swatVeh.CreatePedOnSeat(VehicleSeat.RightRear, PedHash.Cop01SFY);
+			//Give the driver and passengers weapons (Weapon (HASH), ammo, equp the weapon, is ammo loaded in clip)
+			swatDriver.Weapons.Give(WeaponHash.CombatPDW, 500, false, false);
+			swatDriver.Weapons.Give(WeaponHash.CombatPistol, 500, false, false);
+			swatPassengerRightFront.Weapons.Give(WeaponHash.CombatPDW, 500, false, false);
+			swatPassengerRightFront.Weapons.Give(WeaponHash.CombatPistol, 500, false, false);
+			swatPassengerLeftRear.Weapons.Give(WeaponHash.CombatPDW, 500, false, false);
+			swatPassengerLeftRear.Weapons.Give(WeaponHash.CombatPistol, 500, false, false);
+			swatPassengerRightRear.Weapons.Give(WeaponHash.PumpShotgun, 500, false, false);
+			swatPassengerRightRear.Weapons.Give(WeaponHash.CombatPistol, 500, false, false);
+			//Marks them "no longer needed" them so GTA V AI system takes over them
+			swatDriver.MarkAsNoLongerNeeded();
+			swatPassengerRightFront.MarkAsNoLongerNeeded();
+			swatPassengerLeftRear.MarkAsNoLongerNeeded();
+			swatPassengerRightRear.MarkAsNoLongerNeeded();
+		}
 
 		void CreateCopsandVehicle()
 		{
 			//Creates the cop car (The model (HASH), where it spawns in the world (VECTOR 3))
 			Vehicle copVeh = World.CreateVehicle(VehicleHash.Police2, Game.Player.Character.GetOffsetInWorldCoords(new GTA.Math.Vector3(0, 10, 0)));
-            //Place it on the ground
-            copVeh.PlaceOnGround();
-            copVeh.MarkAsNoLongerNeeded();
+			//Place it on the ground
+			copVeh.PlaceOnGround();
+			copVeh.MarkAsNoLongerNeeded();
 			//Creates the driver (seat, model (HASH))
 			Ped copDriver = copVeh.CreatePedOnSeat(VehicleSeat.Driver, PedHash.Cop01SMY);
 			//Creates the passenger (seat, model (HASH))
